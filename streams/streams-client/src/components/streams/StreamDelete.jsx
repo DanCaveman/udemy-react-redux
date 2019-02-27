d4eb9920df6c60e36ec04c6f3c8117f39e17e4c3
DanCaveman <dan@dancaveman.com>
2019-02-26 22:50:01 -0600

    starting on delete - adding modal popup

--- streams/streams-client/src/components/streams/StreamDelete.jsx
+++ streams/streams-client/src/components/streams/StreamDelete.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
+import Modal from '../modal';
+
 const StreamDelete = () => {
-  return (<div>StreamDelete</div>);  
+  return (
+    <div>
+      <Modal header="Delete Stream" content="Are you sure you want to delete this stream?" />
+    </div>
+
+  );  
 };
 
 export default StreamDelete;
