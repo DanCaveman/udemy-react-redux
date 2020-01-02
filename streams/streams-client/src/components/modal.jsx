e3d9a5cda58aa7d2a50be0e75840de2b1551d432
Daniel Kaufman <Daniel.Kaufman@rph.int>
2020-01-02 15:21:10 -0600

    Adding a generic modal to be used with any component.

--- streams/streams-client/src/components/modal.jsx
+++ streams/streams-client/src/components/modal.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import history from '../history';
 
-const Modal = props =>{
+const Modal = props =>{        
     return ReactDOM.createPortal(
-        <div onClick={() => history.push('/')} className="ui dimmer modals visible active"  >
+        <div onClick={props.onDismiss} className="ui dimmer modals visible active"  >
             <div onClick={(e) => e.stopPropagation()} className="ui standard modal visible active" >
                 <div className="header">{props.header}</div>
                 <div className="content">{props.content}</div>
                 <div className="actions">
-                    <button className="ui primary button">Delete</button>
-                    <button className="ui button">Cancel</button>
+                    {props.actions}
                 </div>
             </div>
         </div>,
--- streams/streams-client/src/components/streams/StreamDelete.jsx
+++ streams/streams-client/src/components/streams/StreamDelete.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
-
 import Modal from '../modal';
+import history from '../../history';
 
 const StreamDelete = () => {
+  const actions = (
+    <React.Fragment>
+      <button className="ui button negative">Delete</button>
+      <button onClick={() => history.push('/')} className="ui button">Cancel</button>
+    </React.Fragment>
+  );
   return (
     <div>
-      <Modal header="Delete Stream" content="Are you sure you want to delete this stream?" />
+      <Modal 
+        header="Delete Stream"
+        content="Are you sure you want to delete this stream?"
+        actions={actions}
+        onDismiss={() => history.push('/')}/>
     </div>
 
   );  
